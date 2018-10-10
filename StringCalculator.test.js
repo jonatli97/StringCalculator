d3102c0a3db617d8d7fd4a334b2ba323f6f50d8a
Jón  Atli <jonatli@Jons-MacBook-Pro.local>
2018-10-10 14:56:40 +0000

    Add for 2 numbers

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -2,3 +2,11 @@ const add = require("./calcluator");
 it("should return zero on an empty string", ()=>{
     expect("").toBe(0);
 });
+
+it("should return number when only one number is in the string", ()=>{
+    expect("5").toBe(5);
+});
+
+it("should return sum of two given numbers", ()=>{
+    expect(add("1,2")).toBe(3);
+});
